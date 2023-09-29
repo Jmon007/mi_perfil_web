ec26bef75c1b1965da2c268e2d47e1c4934b8aec
Jmon007 <mario791130@gmail.com>
2023-09-28 19:33:12 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+//alert("Hola este es mi Javascript");
+
+//let nombre = "Mario";
+//nombre = "Jorge";
+
+
+//var nombre1 = "Mario";
+
+
+//const nombre2 = "Mario";
+//nombre2 = "Jorge";
+//console.log (nombre);
+//console.log (nombre1);
+//console.log (nombre2);
+
+//SELECIONAR ELEMENTOS
+
+let contenidoTitulo = "Nombre"
+
+//let titulo = document. querySelector(".logo .fuente")
+//titulo.innerHTML = contenidoTitulo;
+
+//CONDICIONALES
+let textoTitulo = titulo.inerText;
+console.log(textoTitulo);
+
+if (textoTitulo == "Nombre") {
+    titulo.innerHTML = "Otro";
+} else {
+  console.log("no se cumple");
+}
